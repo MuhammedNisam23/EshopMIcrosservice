7236d37fac05d9b03bc75c57a8afac02e13df02b
nisam <nisamth78@gmail.com>
2025-01-22 12:40:33 +0530

    Create API end point developed using carter

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,8 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
 
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseRouting();
+
+// Map Carter endpoints
+app.MapCarter();
+
 
 app.Run();
