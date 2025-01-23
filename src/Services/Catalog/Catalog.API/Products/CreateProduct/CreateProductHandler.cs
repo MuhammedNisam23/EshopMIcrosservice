e6606d06e13d74fb0326286b3eeabd12b0e76555
nisam <nisamth78@gmail.com>
2025-01-23 12:46:00 +0530

    New Configuration Added Docker

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,26 +1,32 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-        : ICommand <CreateProductResult>;
-    public record CreateProductResult(Guid Id);
-    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        public async  Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        //create Product entity from command object
+        //save to database
+        //return CreateProductResult result
+
+        var product = new Product
         {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        
+        //save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
-            var product = new Product
-            {
-                Name = command.Name,
-                Category = command.Category,
-                Description = command.Description,
-                ImageFile = command.ImageFile,
-                Price = command.Price
-            };
-
-
-            //ToDo: Save product to database
-
-            return new CreateProductResult(Guid.NewGuid());
-        }
+        //return result
+        return new CreateProductResult(product.Id);
     }
 }
